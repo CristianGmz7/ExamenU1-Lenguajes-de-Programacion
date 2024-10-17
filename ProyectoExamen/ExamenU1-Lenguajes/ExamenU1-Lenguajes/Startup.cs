b88fc4c55b05c2faf965a45ffcf94cef406ac64f
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2024-10-17 12:00:44 -0600

    init

---
+++ ProyectoExamen/ExamenU1-Lenguajes/ExamenU1-Lenguajes/Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace ExamenU1_Lenguajes;
+
+public class Startup
+{
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        //HttpContext
+
+        //DbContext
+
+        //Servicios de interfaces que se vayan creando
+
+        //Identity
+
+        //AutoMapper
+
+        //Cors relacionado al frontend
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        //useCors
+
+        //useauthentication
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Examen Lenguajes de Programacion I Unidad
+Alumno: Cristian Adonis Gomez 
+Pareja de trabajo: Ana Victoria Henriquez
